a04f5b51126e2a96193619b8c1fc05f3f6abc9fc
Skylarr Laptop <TheOtherSkylarr@gmail.com>
2022-03-31 14:19:52 -0700

    Add crude player movement

--- YouWillExplode/Assets/Scripts/Basic/PlayerMovement.cs
+++ YouWillExplode/Assets/Scripts/Basic/PlayerMovement.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+    public float walkMult = 1f;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();  
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Get axis input
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector3 move = new Vector3(x, y, 0);
+
+        // Move player
+        transform.Translate(move * Time.deltaTime * walkMult);
+
     }
 }
