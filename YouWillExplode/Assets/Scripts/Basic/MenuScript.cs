0124cfd414463cf17c6df6e47efaf467be551939
Skylarr Laptop <TheOtherSkylarr@gmail.com>
2022-03-31 09:42:10 -0700

    Add functional quit button

---
+++ YouWillExplode/Assets/Scripts/Basic/MenuScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        Debug.Log("Playmode stopped. Normally this would quit the game.");
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
