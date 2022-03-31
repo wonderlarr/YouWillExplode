6bc602b9f0a201918dcfac10dc4c9e168ccf4891
Skylarr Laptop <TheOtherSkylarr@gmail.com>
2022-03-31 09:49:50 -0700

    Add crude buildIndex based "Play" button

--- YouWillExplode/Assets/Scripts/Basic/MenuScript.cs
+++ YouWillExplode/Assets/Scripts/Basic/MenuScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
@@ -14,6 +15,11 @@ public class MenuScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void ExitGame()
